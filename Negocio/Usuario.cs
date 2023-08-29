a6df7a2ed8a449e71b6cac6b5e5be587767bc0ff
lautarogerlero <lautarogerlero@gmail.com>
2023-08-29 13:30:58 -0300

    First commit

---
+++ Negocio/Usuario.cs
@@ -0,0 +1,20 @@
+﻿// Contiene las acciones de negocio. Las clases sin atributos, solo con acciones
+// Acá va a estar la clase usuario con todas sus acciones
+// Por ejemplo: la clase usuario con el metodo crearUsuario
+// Realiza las validaciones de negocio (las validaciones especificas de cada clase)
+// El método crearUsuario crea una instancia de la clase Usuario de la capa de Modelo
+// Ve a las otras dos capas
+namespace Negocio
+{
+    public class Usuario
+    {
+        // debe tener un método CrearUsuario que recibe los parametros necesarios para la creación
+        // CrearUsuario tiene que llamar a otro método vValidarUsuario que valide que el nombre de usuarios cumpla los requisitos
+        // Otro método login que permita iniciar sesión con el nombre de usuario y la contraseña. En este caso, si es el primer login
+        // debe solicitar cambiar la contraseña y el estado del usuario (capa Modelo) pasará a ser ACTIVO
+        // Otro método SolicitarContrasenia que pida una nueva contraseña y la guarde
+        // SolicitarContrasenia va a llamar al método ValidarContrasenia que verifique que cumpla los requisitos
+        // Cuando el usuario se quiera registrar, se deberá chequear hace cuanto cambio la contraseña, si pasaron 30 días o más
+        // se deberá llamar nuevamente al método SolicitarContrasenia (chequeando que la nueva contraseña no sea igual a la anterior)
+    }
+}
---
+++ Utils/ConsolaUtils.cs
@@ -0,0 +1,125 @@
+﻿// Parte de la capa de Presentación
+// Contiene las validaciones generales
+
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace Utils
+{
+    public class ConsolaUtils
+    {
+        public static string PedirString(string msg)
+        {
+            // Método para pedir un string
+            Console.WriteLine(msg);
+            string valor = Console.ReadLine();
+
+
+            return valor;
+        }
+
+        public static int PedirInt(string msg)
+        {
+            // Método para pedir un int y verificar que sea un int
+            int valor;
+            bool esNumero;
+            do
+            {
+                Console.WriteLine(msg);
+                esNumero = int.TryParse(Console.ReadLine(), out valor);
+                if (!esNumero)
+                {
+                    Console.WriteLine("Por favor ingrese un número.");
+                }
+            } while (!esNumero);
+            
+            return valor;
+        }
+
+        public static DateTime PedirFecha(string msg)
+        {
+            // Méotodo para pedir una fecha y verificar que esté en formato fecha
+            DateTime valor;
+            bool esFechaValida;
+
+            do
+            {
+                Console.WriteLine(msg + " con el formato dd/MM/yyyy");
+                esFechaValida = DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out valor);
+                
+                if(!esFechaValida)
+                {
+                    Console.WriteLine("Fecha no válida.");
+                }
+            } while(!esFechaValida);
+            
+
+            //Validaciones
+            return valor;
+        }
+
+        public static string ValidarNombre(string msg)
+        {
+            string nombre;
+            bool nombreValido;
+            do
+            {
+                nombre = PedirString(msg);
+                // Chequear que el nombre tenga más de 2 caracteres y solo contenga letras
+                nombreValido = nombre.Length > 2 && Regex.IsMatch(nombre, @"^[a-zA-Z]+$");
+                if(!nombreValido)
+                {
+                    Console.WriteLine("Nombre inválido");
+                }
+            } while(!nombreValido);
+
+            // Convertir la primera letra a mayúscula y las demas a minúscula
+            nombre = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(nombre.ToLower());
+
+            return nombre;
+        }
+
+        public static int ValidarDni(string msg)
+        {
+            int dni;
+            bool dniValido;
+            do
+            {
+                dni = PedirInt(msg);
+                // Verificar que el DNI tenga 8 números
+                dniValido = dni.ToString().Length <= 8;
+                if(!dniValido)
+                {
+                    Console.WriteLine("DNI inválido.");
+                }
+            } while(!dniValido);
+
+            return dni;
+        }
+
+        public static DateTime ValidarFechaNacimiento(string msg)
+        {
+            DateTime fechaNacimiento;
+            bool fechaNacimientoValido;
+            do
+            {
+                fechaNacimiento = PedirFecha(msg);
+                // Verificar que la fecha de nacimiento no sea antes del 1/1/1999 y no sea después de la fecha de hoy menos 10 años.
+                DateTime fechaMinima = new DateTime(1900, 1, 1);
+                DateTime fechaHoy = DateTime.Now;
+                DateTime fechaMaxima = fechaHoy.AddYears(-10); // Restarle 10 años a la fecha de hoy
+                fechaNacimientoValido = fechaNacimiento >= fechaMinima && fechaNacimiento < fechaMaxima;
+                if(!fechaNacimientoValido)
+                {
+                    Console.WriteLine("Fecha de nacimiento inválida");
+                }
+            } while(!fechaNacimientoValido);
+
+            return fechaNacimiento;
+        }
+
+        // Falta el método ValidarEmail
+       
+    }
+}
